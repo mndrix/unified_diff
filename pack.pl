1ba90aeb9591f2b96c6b44061d9fa107cfcae7c3
Michael Hendricks <michael@ndrix.org>
2013-02-23 14:27:00 -0700

    Initial playground

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(unified_diff).
+version('0.0.1').
+title('').
+author('Michael Hendricks','michael@ndrix.org').
+packager('Michael Hendricks','michael@ndrix.org').
+maintainer('Michael Hendricks','michael@ndrix.org').
+home('http://packs.ndrix.com/unified_diff/index.html').
+download('http://packs.ndrix.com/unified_diff/unified_diff-0.0.1.tgz').
---
+++ prolog/unified_diff.pl
@@ -0,0 +1,54 @@
+:- use_module(library(dcg/basics)).
+:- use_module(library(pure_input)).
+
+try(Files) :-
+    phrase_from_file(patch(_,Files), 'a.patch'),
+    !.
+
+patch(Intro, Files) -->
+    intro(Intro),
+    files(Files).
+
+intro(Intro) -->
+    s(Intro), "\n".
+
+files([file(Old,New,Hunks)|Files]) -->
+    file_header(Old, New),
+    %{ format('old=~s new=~s~n', [Old,New]) },
+    hunks(Hunks),
+    files(Files).
+files([]) --> "".
+
+file_header(Old, New) -->
+    "diff --git ", s(Old), " ", s(New), "\n",
+    s(_),  % ignore index line
+    "--- ", s(Old), "\n",
+    "+++ ", s(New), "\n".
+
+hunks([hunk(Before, After, Heading, Deltas)|Hunks]) -->
+    hunk_header(Before, After, Heading),
+    %{ format('b=~s a=~s h=~s~n', [Before,After,Heading]) },
+    deltas(Deltas),
+    hunks(Hunks).
+hunks([]) --> "".
+
+hunk_header(Before, After, Heading) -->
+    "@@ -", s(Before), " +", s(After), " @@ ", s(Heading), "\n".
+hunk_header(Before, After, "") -->
+    "@@ -", s(Before), " +", s(After), " @@\n".
+
+deltas([Delta|Deltas]) -->
+    delta(Delta),
+    deltas(Deltas).
+deltas([]) --> "".
+
+delta(' '(Content)) -->
+    " ", swo(Content, "\n").
+delta(+(Content)) -->
+    "+", swo(Content, "\n").
+delta(-(Content)) -->
+    "-", swo(Content, "\n").
+
+
+s(S) --> string(S).
+swo(S, Stop) --> string_without(Stop, S), Stop.
